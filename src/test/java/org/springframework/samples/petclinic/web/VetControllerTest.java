d36a60dac6ff41784df0764e64f3717114db385f
mayyaannkk <mayankpant.dev@gmail.com>
2026-03-03 10:26:28 +0530

    setup

---
+++ src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.java
@@ -0,0 +1,16 @@
+package org.springframework.samples.petclinic.web;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VetControllerTest {
+
+    @Test
+    void showVetList() {
+    }
+
+    @Test
+    void showResourcesVetList() {
+    }
+}
