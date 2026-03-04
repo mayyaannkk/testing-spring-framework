1fc21291b855f8f980cd4a9bc25f062d1487658a
mayyaannkk <mayankpant.dev@gmail.com>
2026-03-04 16:40:31 +0530

    picking components using component-scan

---
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterComponenetScanTest.java
@@ -0,0 +1,30 @@
+package org.springframework.samples.petclinic.sfg.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.samples.petclinic.sfg.HearingInterpreter;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringJUnitConfig(classes = {HearingInterpreterComponenetScanTest.TestConfig.class})
+class HearingInterpreterComponenetScanTest {
+
+    @Configuration
+    @ComponentScan("org.springframework.samples.petclinic.sfg")
+    static class TestConfig {
+    }
+
+    @Autowired
+    HearingInterpreter hearingInterpreter;
+
+    @Test
+    void whatIHeard() {
+        String name = hearingInterpreter.whatIHeard();
+
+        assertEquals("Laurel", name);
+    }
+}
