f08ad95bbd2c2b0c3ea1da5efae8fa94f86fe7dc
mayyaannkk <mayankpant.dev@gmail.com>
2026-03-04 14:02:22 +0530

    using spring context in tests

--- src/test/java/org/springframework/samples/petclinic/sfg/junit4/HearingInterpreterTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit4/HearingInterpreterTest.java
@@ -2,19 +2,25 @@ package org.springframework.samples.petclinic.sfg.junit4;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.sfg.BaseConfig;
 import org.springframework.samples.petclinic.sfg.HearingInterpreter;
+import org.springframework.samples.petclinic.sfg.LaurelConfig;
 import org.springframework.samples.petclinic.sfg.LaurelWordProducer;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {BaseConfig.class, LaurelConfig.class})
 public class HearingInterpreterTest {
 
+    @Autowired
     HearingInterpreter hearingInterpreter;
-
-    @Before
-    public void setUp() throws Exception {
-        hearingInterpreter = new HearingInterpreter(new LaurelWordProducer());
-    }
 
     @Test
     public void whatIHeard() {
