119ed11d062386976c876d65f1778ef9744baa2e
mayyaannkk <mayankpant.dev@gmail.com>
2026-03-04 18:56:39 +0530

    using active profiles

--- src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterComponentScanTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterComponentScanTest.java
@@ -2,17 +2,20 @@ package org.springframework.samples.petclinic.sfg.junit5;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.samples.petclinic.sfg.HearingInterpreter;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringJUnitConfig(classes = {HearingInterpreterComponenetScanTest.TestConfig.class})
-class HearingInterpreterComponenetScanTest {
+@ActiveProfiles("component-test")
+@SpringJUnitConfig(classes = {HearingInterpreterComponentScanTest.TestConfig.class})
+class HearingInterpreterComponentScanTest {
 
+    @Profile("component-test")
     @Configuration
     @ComponentScan("org.springframework.samples.petclinic.sfg")
     static class TestConfig {
