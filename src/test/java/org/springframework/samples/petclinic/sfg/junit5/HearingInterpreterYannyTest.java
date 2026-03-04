384691435082c4753e852e9bfe455208318d8b0f
mayyaannkk <mayankpant.dev@gmail.com>
2026-03-04 16:33:08 +0530

    inner class for test config

--- src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterYannyTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterYannyTest.java
@@ -1,7 +1,10 @@
-package org.springframework.samples.petclinic.sfg;
+package org.springframework.samples.petclinic.sfg.junit5;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.sfg.BaseConfig;
+import org.springframework.samples.petclinic.sfg.HearingInterpreter;
+import org.springframework.samples.petclinic.sfg.YannyConfig;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.junit.jupiter.api.Assertions.*;
